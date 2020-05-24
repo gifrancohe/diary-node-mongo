9f6ffabe81b9323c69fe61ac810fc60db566c293
Giovanny Franco <giovannyfranco@mattelsa.net>
2020-05-24 13:40:05 -0500

    Create a server nodejs

---
+++ index.js
@@ -0,0 +1,19 @@
+var bodyParser   = require('body-parser'),
+    http         = require('http'),
+    express      = require('express')  
+
+var port         = port = process.env.PORT || 3000, 
+    app          = express(),
+    Server       = http.createServer(app)     
+
+var Routing = require('./data/requestRounting.js')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+app.use('/searcher', Routing)
+
+Server.listen(port, function(){
+  console.log('Server is listeing in port: ' + port)
+})
